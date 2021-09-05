47d90aa5d526ef14de1967bb4c6a267a0dff5344
Keith Hayes <klhayes@comcast.net>
2021-09-05 16:43:00 -0700

    moved gitignore

---
+++ build/CMakeFiles/showpic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/showpic.dir/src/main.cpp.o"
+  "CMakeFiles/showpic.dir/src/picwindow.cpp.o"
+  "showpic"
+  "showpic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/showpic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
