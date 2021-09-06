6ca669e00ab629aab6c711d3c7a0e89fd3a5c496
Keith Hayes <klhayes@comcast.net>
2021-09-06 00:08:28 -0700

    no more .bmp files

---
+++ build/CMakeFiles/showpic.a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/showpic.a.dir/src/main.cpp.o"
+  "CMakeFiles/showpic.a.dir/src/picwindow.cpp.o"
+  "showpic.a"
+  "showpic.a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/showpic.a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
